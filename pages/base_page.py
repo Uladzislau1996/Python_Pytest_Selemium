04f0503463d6627ac47428726fd52dfa626ac88c
Vlad Galushkevich <mr.halushkevich@gmail.com>
2022-01-31 20:08:00 +0300

    First test with Page Object

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,13 +1,6 @@
-from nturl2path import url2pathname
-from webbrowser import BaseBrowser
-
-
 class BasePage():
-    
     def __init__(self, browser, url):
         self.browser = browser 
         self.url = url
-
-    
     def open(self):
         self.browser.get(self.url)
