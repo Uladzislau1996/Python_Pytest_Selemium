2300fdbc65c531594c113c8c0385bf3d4290e6a5
Vlad Galushkevich <mr.halushkevich@gmail.com>
2022-02-09 20:06:21 +0300

    Add test for basket button

--- pages/locators.py
+++ pages/locators.py
@@ -1,9 +1,11 @@
 from selenium.webdriver.common.by import By
 
 class MainPageLocators():
-    LOGIN_LINK = (By.CSS_SELECTOR, "#registration_link")
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    ADD_TO_BASKET_BUTTON = (By.CLASS_NAME, "btn-add-to-basket")
 
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
     
+ 
--- test_main_page.py
+++ test_main_page.py
@@ -1,13 +1,27 @@
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
+from selenium.common.exceptions import NoAlertPresentException
+import math
 
 def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer"
+    link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)   
     page.open()                      
-    page.go_to_login_page()          
+    page.go_to_login_page()      
+    login_page = page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()   
 
 def test_guest_should_see_login_link(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer"
+    link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
+
+def test_guest_can_add_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+    page = MainPage(browser, link)
+    page.open()
+    page.add_to_basket()
+    page.solve_quiz_and_get_code()
+
